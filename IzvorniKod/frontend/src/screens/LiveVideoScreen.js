0b880c36e4da8b2a443d7c0113c2e4d050fd83ab
kore <korinajuric03@gmail.com>
2023-11-06 23:01:58 +0100

    live video screen added

--- IzvorniKod/frontend/src/screens/LiveVideoScreen.js
+++ IzvorniKod/frontend/src/screens/LiveVideoScreen.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from ".././components/Header.js";
 import Footer from "../components/Footer";
-
+import "../css/liveVideo.css";
 import "../css/main.css";
+import LiveVideo from "../components/LiveVideo";
 
 const PosterScreen = () => {
   return (
     <div>
       <Header />
       <h1>Live Video prijenos</h1>
+      <LiveVideo />
       <Footer />
     </div>
   );
