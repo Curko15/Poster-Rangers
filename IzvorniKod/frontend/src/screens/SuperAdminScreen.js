5d2cd3d8c2154c94d085eb443f134e65227640ce
DariaBevanda <daria.bevanda@rapidhealth.co.uk>
2023-11-14 11:57:15 +0100

    Uklonjena redundantna komponenta za dodavanje konferencije

--- IzvorniKod/frontend/src/screens/SuperAdminScreen.js
+++ IzvorniKod/frontend/src/screens/SuperAdminScreen.js
@@ -2,19 +2,19 @@ import React from "react";
 import Header from ".././components/Header.js";
 import Footer from "../components/Footer";
 import "../css/main.css";
-import AddAdmin from "../components/AddAdmin.js"; 
-import AddConference from "../components/AddConference.js";
+import AddAdmin from "../components/AddAdmin.js";
+import AddConferenceComponent from "../components/AddConferenceComponent.js";
 
 const SuperAdminScreen = () => {
-    return (
-        <div>
-        <Header />
-        <h1>Dobrodošao superadministratore!</h1>
-        <AddAdmin />
-        <AddConference />
-        <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <h1>Dobrodošao superadministratore!</h1>
+      <AddAdmin />
+      <AddConferenceComponent />
+      <Footer />
+    </div>
+  );
 };
 
 export default SuperAdminScreen;
