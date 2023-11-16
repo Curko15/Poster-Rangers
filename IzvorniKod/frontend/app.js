7138fa643526fb2e9014042df323580d20127a29
DariaBevanda <daria.bevanda@rapidhealth.co.uk>
2023-11-16 12:54:23 +0100

    Deploy-setup14

--- IzvorniKod/frontend/app.js
+++ IzvorniKod/frontend/app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 
 // Configuration
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const HOST = "localhost";
 //const { API_BASE_URL } = "https://poster-rangers-be.onrender.com";
 
