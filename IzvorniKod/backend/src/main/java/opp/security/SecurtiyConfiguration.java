407e0e85816644af2c66bcce33cdbe673e940c3f
ll54152 <ll54152@fer.hr>
2023-12-27 12:39:27 +0100

    Popravljeno lokalno dizanje aplikacije

--- IzvorniKod/backend/src/main/java/opp/security/SecurtiyConfiguration.java
+++ IzvorniKod/backend/src/main/java/opp/security/SecurtiyConfiguration.java
@@ -40,6 +40,7 @@ public class SecurtiyConfiguration {
                     CorsConfiguration corsConfig = new CorsConfiguration();
                     corsConfig.setAllowCredentials(true);
                     corsConfig.addAllowedOriginPattern("https://poster-rangers-fe.onrender.com");
+                    corsConfig.addAllowedOriginPattern("http://localhost:3000/");
                     corsConfig.addAllowedHeader("Authorization");
                     corsConfig.addAllowedHeader("Content-Type");
                     corsConfig.addAllowedMethod(HttpMethod.GET);
