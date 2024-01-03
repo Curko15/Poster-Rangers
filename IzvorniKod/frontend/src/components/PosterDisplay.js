48d5a8d09e986f33dae20264e8f8314f28a72f24
DariaBevanda <daria.bevanda@rapidhealth.co.uk>
2024-01-03 15:16:34 +0100

    Deploy changes

--- IzvorniKod/frontend/src/components/PosterDisplay.js
+++ IzvorniKod/frontend/src/components/PosterDisplay.js
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { getAuthToken, getConferenceId } from "../services/AuthService";
 import axios from "axios";
 
 import "../css/posterDisplay.css";
 
-const PosterDisplay = async () => {
+const PosterDisplay = () => {
+  //const backendURL = "http://localhost:8081/static/";
   const [images, setImages] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`/api/getAll/${getConferenceId()}`, {
+        const response = await axios.get(`/api/poster/getAll/21`, {
           headers: {
-            Authorization: "Bearer " + getAuthToken().token,
+            "Content-Type": "application/json",
           },
         });
         setImages(response.data);
+        console.log(response.data);
       } catch (error) {
         console.error("Error:", error);
         setImages([]);
@@ -32,10 +33,17 @@ const PosterDisplay = async () => {
       ) : (
         images.map((image, index) => (
           <div key={index}>
-            <img className="poster" src={image} alt={`poster-${index}`} />
+            <img
+              className="poster"
+              src={encodeURIComponent(image.posterPath)}
+              alt={`poster-${index}`}
+            />
             <div className="details">
-              <h3 className="title">Naziv</h3>
-              <h3 className="author">Autor</h3>
+              <h3 className="title">Naziv: {image.nazivPoster}</h3>
+              <h3 className="author">
+                Autor: {`${image.imeAutor} ${image.prezimeAutor}`}
+              </h3>
+              <h3 className="email">Email autora: {image.emailAutor}</h3>
             </div>
           </div>
         ))
