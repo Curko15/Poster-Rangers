be3aa2881638be6baee63ad1e5241b42d86fc36f
ll54152 <ll54152@fer.hr>
2024-01-09 18:21:18 +0100

    Poredak sada vraća mapu koja ima i postere koji imaju nula glasova

--- IzvorniKod/backend/src/main/java/opp/service/impl/GlasanjeServiceJPA.java
+++ IzvorniKod/backend/src/main/java/opp/service/impl/GlasanjeServiceJPA.java
@@ -2,9 +2,11 @@ package opp.service.impl;
 
 import jakarta.transaction.Transactional;
 import opp.dao.GlasanjeRepo;
+import opp.dao.PosterRepo;
 import opp.domain.Glasanje;
 import opp.domain.Konferencija;
 import opp.domain.Korisnik;
+import opp.domain.Poster;
 import opp.service.GlasanjeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,9 @@ import java.util.stream.Collectors;
 public class GlasanjeServiceJPA implements GlasanjeService {
     @Autowired
     private GlasanjeRepo glasanjeRepo;
+
+    @Autowired
+    private PosterRepo posterRepo;
 
     @Override
     public Glasanje save(Glasanje glas) {return glasanjeRepo.save(glas);}
@@ -43,6 +48,17 @@ public class GlasanjeServiceJPA implements GlasanjeService {
                     poredak.put(glas.getPosterId(), 1);
                 } else {
                     poredak.put(glas.getPosterId(), poredak.get(glas.getPosterId()) + 1);
+                }
+            }
+
+        }
+
+        List<Poster> ostali = posterRepo.findAll();
+        for (Poster poster : ostali){
+            if(Objects.equals(poster.getKonferencija().getKonfid(), konfid)){
+
+                if(!poredak.containsKey(poster.getPosterId())){
+                    poredak.put(poster.getPosterId(), 0);
                 }
             }
 
